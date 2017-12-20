9815cda32c1f55d59b4a31dd4ce45dd79daea0f4
jpwebdev <jjpwebdev@gmail.com>
2017-12-19 20:04:13 -0500

    images change depending on user choice, firebase: gets names for both players but only the choice of the first one. I also added a turn variable in the db

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,17 +18,19 @@ $(function() {
     // Firebase Reference collections
     const p1Ref = db.ref().child('/p1'); // P1 folder
     const p2Ref = db.ref().child('/p2'); // P2 folder
+    const turnRef = db.ref().child('/turn'); // to track the turns
     const connectionsRef = db.ref("/connections"); // Folder to store each connection
     const connectedRef = db.ref(".info/connected");// Firebase's default Ref to track connections (boolean)
 
     // Variables
     let pNameVal = '';
-    const p1Wins = 0;
-    const p1Losses = 0;
-    const p1Choice = '';
-    const p2Wins = 0;
-    const p2Losses = 0;
-    const p2Choice = '';
+    let p1Wins = 0;
+    let p1Losses = 0;
+    let p1Choice = '';
+    let p2Wins = 0;
+    let p2Losses = 0;
+    let p2Choice = '';
+    let turn = 'p1turn';
 
     // DOM caching
     const $lego = $('#lego');
@@ -36,8 +38,8 @@ $(function() {
     const $pOneNameSpan = $('span.playerOneName');
     const $p1choice = $('#p1ChoiceDiv');
     const $p2choice = $('#p2ChoiceDiv');
-
- 
+    const $imgP1 = $('#playerOne').find('img');
+    const $imgP2 = $('#playerTwo').find('img');;
 
     // Functions
     const playerName = () => {
@@ -60,8 +62,16 @@ $(function() {
                     wins: p1Wins,
                     losses: p1Losses,
                 };
-                // Sync object changes
+                const t = { whoseturn: turn };
+                // Sync object
                 p1Ref.set(p1);
+                turnRef.set(t);
+
+                // Wait for player two
+                console.log('Waiting for player 2');
+                turn = 'p2turn';
+                turnRef.update({ whoseturn: turn }); // Update the turn in the db
+
             }
             else if(activeP == 2) {
                 // Create the object
@@ -71,14 +81,23 @@ $(function() {
                     wins: p2Wins,
                     losses: p2Losses,
                 };
-                // Sync object changes
+                // Sync object
                 p2Ref.set(p2);
+                console.log('play now');
+                turn = 'p1turn';
+                turnRef.update({ whoseturn: turn });
             }
         });
     }
 
-    const getP1Choice = e => {
-        p1Choice = $(e.target).attr('data-userChoice');    // Get p1 choice
+    const getP1Choice = e => {  // Save user choice to Firebase
+        if (turn == 'p2turn') {
+
+        }
+        let leTarget = $(e.target);
+        p1Choice = leTarget.attr('data-userChoice');    // Get p1 choice
+        $imgP1.attr('src', `./assets/imgs/${p1Choice}.png`); // Change the img to match the user's choice
+        p1Ref.update({ choice: p1Choice }); //Update the user choice
     }
 
 
