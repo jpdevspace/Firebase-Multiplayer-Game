5db329aeffb3887e23e5968fe62e8107156fada5
jpwebdev <jjpwebdev@gmail.com>
2017-12-19 19:00:28 -0500

    re wrote the code, now everything is in plain old js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
-(function($) {
-    // This is non-DOM dependant so it can go up here
+$(function() {
+    $('#startGame').modal('show');  // Open the Bootrap 4 modal on start
+
     const config = {
         apiKey: "AIzaSyAjFVg3kJbDmAhjdxTarhD4d7u0tR7xabs",
         authDomain: "rps-homework-a5b81.firebaseapp.com",
@@ -7,92 +8,81 @@
         projectId: "rps-homework-a5b81",
         storageBucket: "",
         messagingSenderId: "648767764205"
-      };
-      // Firebase database init
-      firebase.initializeApp(config);
+    };
+    // Firebase database init
+    firebase.initializeApp(config);
 
-      // Reference the database
-      const db = firebase.database();
+    // Reference the database
+    const db = firebase.database();
 
-    $(function() {
-        const game = {
-            pNameVal: '',
-            p1Wins: 0,
-            p1Losses: 0,
-            p1Choice: '',
-            p2Wins: 0,
-            p2Losses: 0,
-            p2Choice: '',
+    // Firebase Reference collections
+    const p1Ref = db.ref().child('/p1'); // P1 folder
+    const p2Ref = db.ref().child('/p2'); // P2 folder
+    const connectionsRef = db.ref("/connections"); // Folder to store each connection
+    const connectedRef = db.ref(".info/connected");// Firebase's default Ref to track connections (boolean)
 
-            startModal() {
-                $('#startGame').modal('show');  // Open the Bootrap 4 modal on start
-                this.dom_cache();
-                this.event_binders();
-                // this.firebase_init();
-            },
-            dom_cache(){
-                this.$lego = $('#lego');
-                this.$pName = $('#name');
-                this.$pOneNameSpan = $('span.playerOneName');
-                this.$p1choice = $('#p1ChoiceDiv');
-                this.$p2choice = $('#p2ChoiceDiv');
-            },
-            event_binders(){
-                this.$lego.on('click', this.playerName.bind(this));
-                this.$p1choice.on('click', this.p1Choice.bind(this));
+    // Variables
+    let pNameVal = '';
+    const p1Wins = 0;
+    const p1Losses = 0;
+    const p1Choice = '';
+    const p2Wins = 0;
+    const p2Losses = 0;
+    const p2Choice = '';
 
-            },
-            playerName(){
-                this.pNameVal = this.$pName.val();
-                this.firebase_init(); // Init firebase AFTER you have the name
-            },
-            p1Choice(e) {
-                this.p1Choice = $(e.target).attr('data-userChoice');    // Get p1 choice
-                
-            },            
-            firebase_init(){
-                // Create references
-                const p1Ref = db.ref().child('/p1'); // P1 folder
-                const p2Ref = db.ref().child('/p2'); // P2 folder
-                const connectionsRef = db.ref("/connections"); // Folder to store each connection
-                const connectedRef = db.ref(".info/connected");// Firebase's default Ref to track connections (boolean)
+    // DOM caching
+    const $lego = $('#lego');
+    const $pName = $('#name');
+    const $pOneNameSpan = $('span.playerOneName');
+    const $p1choice = $('#p1ChoiceDiv');
+    const $p2choice = $('#p2ChoiceDiv');
 
-                connectedRef.on('value', (snap) => { // Check if someone connected/disconnected
-                    if(snap.val()){ // If someone connected
-                        const justConnected = connectionsRef.push(true);
-                        justConnected.onDisconnect().remove(); // Remove user from the connection list when they disconnect
-                    }
-                });
+ 
 
-                connectionsRef.on('value', (snap) => { // If I just moved someone to my connection folder
-                    console.log(snap.numChildren()); 
-                    const activeP = snap.numChildren();
-                    if(activeP == 1) { // If you're the 1st player
-                        // Create the object
-                        const p1 = {
-                            choice: '',
-                            name: this.pNameVal,
-                            wins: this.p1Wins,
-                            losses: this.p1Losses,
-                        };
-                        // Sync object changes
-                        p1Ref.set(p1);
-                    }
-                    else if(activeP == 2) {
-                        // Create the object
-                        const p2 = {
-                            choice: '',
-                            name: this.pNameVal,
-                            wins: this.p2Wins,
-                            losses: this.p2Losses,
-                        };
-                        // Sync object changes
-                        p2Ref.set(p2);
-                    }
-                });
-            },
+    // Functions
+    const playerName = () => {
+        pNameVal = $pName.val(); // Get the name of the user
 
-        }
-        game.startModal();
-    })
-})(jQuery);
+        connectedRef.on('value', (snap) => { // Check if someone connected/disconnected
+            if(snap.val()){ // If someone connected
+                const justConnected = connectionsRef.push(true);
+                justConnected.onDisconnect().remove(); // Remove user from the connection list when they disconnect
+            }
+        });
+        connectionsRef.on('value', (snap) => { // If I just moved someone to my connection folder
+            console.log(snap.numChildren()); 
+            const activeP = snap.numChildren();
+            if(activeP == 1) { // If you're the 1st player
+                // Create the object
+                const p1 = {
+                    choice: '',
+                    name: pNameVal,
+                    wins: p1Wins,
+                    losses: p1Losses,
+                };
+                // Sync object changes
+                p1Ref.set(p1);
+            }
+            else if(activeP == 2) {
+                // Create the object
+                const p2 = {
+                    choice: '',
+                    name: pNameVal,
+                    wins: p2Wins,
+                    losses: p2Losses,
+                };
+                // Sync object changes
+                p2Ref.set(p2);
+            }
+        });
+    }
+
+    const getP1Choice = e => {
+        p1Choice = $(e.target).attr('data-userChoice');    // Get p1 choice
+    }
+
+
+   // Event Binders
+   $lego.on('click', playerName);
+   $p1choice.on('click', getP1Choice);
+});
